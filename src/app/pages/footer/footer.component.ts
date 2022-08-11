8df7ca97baf80659b3bf3fa7c46adbfcf1c2526a
Anak Agung Gede Putra Jagadhita <gungdehita@gmail.com>
2022-08-11 17:35:41 +0700

    klp1-frontend

--- src/app/pages/footer/footer.component.ts
+++ src/app/pages/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {freeSet} from "@coreui/icons";
 
 @Component({
   selector: 'app-footer',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
+
+  icons = freeSet;
 
   constructor() { }
 
